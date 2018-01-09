f648d2578b4ec73383a3de1daac0c6b990e04b45
John Spens <johns@jane.com>
2018-01-09 11:14:58 -0700

    Package propery updates

--- SiftScience/Properties/AssemblyInfo.cs
+++ SiftScience/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SiftScience")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SiftScience is a .NET class library for the SiftScience web api.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SiftScience")]
